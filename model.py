21ea772e992bd378a2ec6ce90a29ebed560ae149
ClemBqd <clementine.bourquard@gmail.com>
2020-01-23 00:07:40 +0100

    Add function to increase global variable of model kapital_households (sum of households's kapital) and kapital_households_speculators at every step

--- model.py
+++ model.py
@@ -11,13 +11,14 @@ class BtcModel(Model):
         # Chose a schedule
         self.schedule = RandomActivation(self)
         self.production = 10
+        self.kapital_households = 0
+        self.kapital_households_speculators = 0
 
         # Create  a bank, a firm and n household
         # self.bank = Bank(1, self) 
         # self.schedule.add(bank)
         # firm = Firm(2, self)
         # self.schedule.add(firm)
-        
     
         for i in range(self.n_households):
             h = Household(i+2, self)
@@ -27,9 +28,20 @@ class BtcModel(Model):
 
         # Put variable comun of all the model too
 
+    def increase_kapital_households(self):
+        for i in self.schedule.agents:
+            if i.risk_profile == -1:
+                self.kapital_households += i.kapital
+            else:
+                self.kapital_households_speculators += i.kapital
+
+
+
+
     def step(self):
         # Tell all the agents in the model to run their step function
         self.schedule.step()
+        self.increase_kapital_households()
         #self.bank.step()
         # Collect data
         # self.datacollector.collect(self)
