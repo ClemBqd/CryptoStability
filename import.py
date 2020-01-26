0682d209bf5e53801dcbde81ab85acb41c43c23a
jbzerlamenace <jean-baptiste.bernard@edu.ece.fr>
2020-01-27 00:20:01 +0100

    dataframe

--- import.py
+++ import.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 
-df3 = pd.read_csv('pfebtc.csv',sep = ',')   
-print(df3)
-print(df3.head())
+df3 = pd.read_csv('pfebtc.csv',sep = ',')  
+print(df3) 
+current_time="28/1/2015"
+print(df3.loc[df3['Date'] == current_time].index.item())
+print(df3['cours'][2])
