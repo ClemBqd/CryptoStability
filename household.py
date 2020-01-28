272514a6a846359c80296e0efcfad7057cf6afde
jbzerlamenace <jean-baptiste.bernard@edu.ece.fr>
2020-01-28 23:14:34 +0100

    formule change

--- household.py
+++ household.py
@@ -5,7 +5,7 @@ import pandas as pd
 from datetime import datetime, timedelta
 
 
-df3 = pd.read_excel('pfebtc.xltx') 
+df3 = pd.read_excel('pfebtc.xlsx') 
 
 class Household(Agent):
     def __init__(self, unique_id, risk_profile, P, model):
@@ -16,30 +16,28 @@ class Household(Agent):
         self.wage = 0 #salary month
         self.kapital = 7.50 # Maybe to /12
         self.conso = 0
-        self.speculator_portfolio = 1
+        self.speculator_portfolio= 0
         self.loan = 0 
         
     def kapital_evolution(self):
         if self.risk_profile != -1:
-            self.kapital = (1 + rk/self.model.n)*self.kapital + self.wage - self.conso - self.loan/(3*self.model.n)- self.loan*rate_loan_h/self.model.n+(1 + rk/self.model.n)*
+            self.kapital = (1 + rk/self.model.n)*self.kapital*(1-self.P) + self.wage - self.conso - self.loan/(3*self.model.n)- self.loan*rate_loan_h/self.model.n+self.speculator_portfolio
         else:    
             self.kapital = (1 + rk/self.model.n)*self.kapital + self.wage - self.conso - self.loan/(3*self.model.n)- self.loan*rate_loan_h/self.model.n
         return self.kapital
     
     def speculator_ptf(self):
         if self.risk_profile != -1:
-            self.speculator_portfolio = self.speculator_portfolio*(1 +df3['variation'][df3.loc[df3['Date'] == self.model.current_datetime].index.item()]) + self.P*self.kapital
+            self.speculator_portfolio = self.P*self.kapital*(1 +df3['variation'][df3.loc[df3['Date'] == self.model.current_datetime].index.item()])
         return self.speculator_portfolio
     
     def consumption(self):
-        if self.risk_profile == -1:
-            self.conso = self.model.techno*(self.kapital**self.model.alpha)*(self.model.travail**(1-self.model.alpha))*(1 - self.model.alpha*self.model.beta)
-        else:
-            self.conso = self.model.techno*((self.kapital)**self.model.alpha)*(self.model.travail**(1-self.model.alpha))*(1 - self.model.alpha*self.model.beta)
+        self.conso = self.model.techno*((self.kapital)**self.model.alpha)*((self.model.travail/self.model.n_households)**(1-self.model.alpha))*(1 - self.model.alpha*self.model.beta)
         
     def receive_salary(self):
         self.wage = (1 - self.model.gamma)*self.model.production
         return self.wage
+    
 
     def step(self):
         self.speculator_ptf()
--- test3.py
+++ test3.py
@@ -6,9 +6,9 @@ import pandas
 import matplotlib.pyplot as plt 
 from pandas import DataFrame
 
-empty_model = BtcModel(20)
+empty_model = BtcModel(10)
 
-for i in range(2):
+for i in range(20):
     empty_model.step()
 
 households_kapital = [a.kapital for a in empty_model.schedule.agents]
