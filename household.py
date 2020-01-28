c2569558e0f2ecc00a0b00958450416fda9fa001
ClemBqd <clementine.bourquard@gmail.com>
2020-01-28 17:49:55 +0100

    wip

--- household.py
+++ household.py
@@ -5,19 +5,19 @@ import pandas as pd
 from datetime import datetime, timedelta
 
 
-P = 0.1 #Propertion to speculate
 df3 = pd.read_excel('pfebtc.xltx') 
 
 class Household(Agent):
-    def __init__(self, unique_id, risk_profile, model):
+    def __init__(self, unique_id, risk_profile, model, P):
         self.unique_id = unique_id
         self.risk_profile = risk_profile # -1 for risk_averse 1 for risk_lover and later 0 for risk_neutral
+        self.P = P # Propertion to speculate
         super().__init__(unique_id, model)
         self.wage = 0 #salary month
-        self.kapital = 7.50
+        self.kapital = 7.50 # Maybe to /12
         self.conso = 0
         self.speculator_portfolio = 1
-        self.loan = 0
+        self.loan = 0 
         
     def kapital_evolution(self):
         self.kapital = (1 + rk/self.model.n)*self.kapital + self.wage - self.conso - self.loan*(1 + rate_loan_h)/self.model.n
@@ -25,7 +25,7 @@ class Household(Agent):
     
     def speculator_ptf(self):
         if self.risk_profile != -1:
-            self.speculator_portfolio = self.speculator_portfolio*(1 +df3['variation'][df3.loc[df3['Date'] == self.model.current_datetime].index.item()]) + P*self.kapital
+            self.speculator_portfolio = self.speculator_portfolio*(1 +df3['variation'][df3.loc[df3['Date'] == self.model.current_datetime].index.item()]) + self.P*self.kapital
         return self.speculator_portfolio
     
     def consumption(self):
